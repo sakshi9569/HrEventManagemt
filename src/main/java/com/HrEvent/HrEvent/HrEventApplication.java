8acca5ac516164e1b8f36ee8ceb563c08f678fe0
User OnGrid <userongrid@Admins-MacBook-Pro-5.local>
2025-01-29 17:30:13 +0530

    Initial commit

---
+++ src/main/java/com/HrEvent/HrEvent/HrEventApplication.java
@@ -0,0 +1,13 @@
+package com.HrEvent.HrEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HrEvent/HrEvent/HrEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HrEvent.HrEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
